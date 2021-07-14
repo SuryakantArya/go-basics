22b1df724dd351ee21e7b9533fe06a87716f0ef8
SuryakantArya <72564959+SuryakantArya@users.noreply.github.com>
2021-07-14 15:37:36 -0400

    Update main.go
    test commit

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	for x := 65; x <= 90; x++ {
 		for j := 1; j <= 3; j++ {
 			fmt.Println(j)
+			
+			fmt.Printf("\t%#U\n", x)
+			fmt.Printf("\t%#U\n", x)
 			fmt.Printf("\t%#U\n", x)
 			fmt.Printf("\t%#U\n", x)
 			fmt.Printf("\t%#U\n", x)
